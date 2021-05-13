23ab6dc0acfe29b2d7fed5e5996fddeb99b76e89
Ruturaj Apte <ruturaj.iitb@gmail.com>
2021-05-14 00:00:36 +0530

    minor refactors

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ import requests
 
 def sendImage():
     files = {'file': open('images/dog.jpeg', 'rb')}
-    #url = 'https://flask-service.bov0er98st5ta.us-east-1.cs.amazonlightsail.com/qure/v1.0/classify/'
-    url = 'http://localhost:5000/qure/v1.0/classify/'
+    url = 'https://flask-service.bov0er98st5ta.us-east-1.cs.amazonlightsail.com/qure/v1.0/classify/'
+    #url = 'http://localhost:5000/qure/v1.0/classify/'
     response = requests.post(url, files=files)
     try:
         print (response.json())
