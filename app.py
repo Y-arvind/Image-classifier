ecdf90c0bb1369227783739aa4b055c9c10f008b
Ruturaj Apte <ruturaj.iitb@gmail.com>
2021-05-13 21:09:22 +0530

    error handling for missing file

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!flask/bin/python
-import os
 from flask import Flask, request
 from resnet_classifier import ResnetClassifier
 
@@ -7,20 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World! Deployed classifier"
+    return "Hello, World!"
 
 @app.route('/qure/v1.0/classify/', methods=['GET','POST'])
 def classify_image():
-    if 'media' not in request.files:
-        print("No file in request")
-    file = request.files['media']
-    #file.save(os.path.join('', 'upload.jpeg'))
+    if 'file' not in request.files:
+        return "Please attach in image", 400
+    file = request.files['file']
     img_bytes = file.read()
     classifier = ResnetClassifier()
     res = classifier.classify(img_bytes=img_bytes)
     return { res[0][0] : res[0][1] }, 200
 
 if __name__ == "__main__":
-	print(("* Loading Keras model and Flask starting server..."
+	print(("* Starting Flask server..."
 		"please wait until server has fully started"))
 	app.run(debug=True, host='0.0.0.0')
--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 import requests
 
 def sendImage():
-    files = {'media': open('cat.jpeg', 'rb')}
+    #files = {'file': open('cat.jpeg', 'rb')}
+    files = {}
     #url = 'https://flask-service.bov0er98st5ta.us-east-1.cs.amazonlightsail.com/qure/v1.0/classify/'
     url = 'http://localhost:5000/qure/v1.0/classify/'
     response = requests.post(url, files=files)
-    print(response.json())
+    try:
+        print (response.json())
+    except:
+        print(response)
 
 sendImage()
